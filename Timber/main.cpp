ce9c158c4875f225a6aa36f100e9d4ddcc5b0342
lakhara <siddharth.lakhara@gmail.com>
2020-12-23 10:19:08 +0530

    Initial Commit

---
+++ Timber/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Timber
+//
+//  Created by slakhara on 23/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
